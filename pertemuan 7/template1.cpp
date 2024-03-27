5ed409ba7f410fcf49ade1cfc974d7cff260a916
dwiandini01 <andinii0204@gmail.com>
2024-03-27 10:36:49 +0700

    pertemuan 7

--- pertemuan 7/template1.cpp
+++ pertemuan 7/template1.cpp
@@ -12,7 +12,7 @@ int main() {
     int a = 10, b = 20;
     double x = 2.5, y = 7.5;
 
-    cout << jumlah(a,b)<< endl;
-    cout << jumlah(x,y)<< endl;
+    cout << jumlah(a,b) << endl;
+    cout << jumlah(x,y) << endl;
 
 }
